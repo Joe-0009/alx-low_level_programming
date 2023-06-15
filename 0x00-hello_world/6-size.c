e38eb08b43b0a072dfcd1af219a93f66ff211ba8
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-15 14:47:31 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n
-	Size of an int: 4 byte(s)\n
-	Size of a long int: 4 byte(s)\n
-	Size of a long long int: 8 byte(s)\n
-	Size of a float: 4 byte(s)");
+	printf("Size of a char: %zu byte(s)\n" sizeof(char));
+	printf("Size of a char: %zu byte(s)\n" sizeof(int));
+	printf("Size of a char: %zu byte(s)\n" sizeof(long int));
+	printf("Size of a char: %zu byte(s)\n" sizeof(long long int));
+	printf("Size of a char: %zu byte(s)\n" sizeof(float));
+
 	return (0);
 }

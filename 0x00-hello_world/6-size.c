caf5b717eff5f44866e9732209ad081b37c4ac7a
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-15 14:40:55 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints size of characters
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n"+"
-Size of an int: 4 byte(s)\n"+"
-Size of a long int: 4 byte(s)\n"+"
-Size of a long long int: 8 byte(s)\n"+
-"Size of a float: 4 byte(s)";
+	printf("Size of a char: 1 byte(s)\n"+
+	"Size of an int: 4 byte(s)\n"+
+	"Size of a long int: 4 byte(s)\n"+
+	"Size of a long long int: 8 byte(s)\n"+
+	"Size of a float: 4 byte(s)");
 	return (0);
 }

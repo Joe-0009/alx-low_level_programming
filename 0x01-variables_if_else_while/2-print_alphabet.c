64aa7555e959fadf5d58dbe20415111065f0fbbe
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-16 12:48:31 -0700

    fff

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alaphabet 
+ *
+ * return : always 0 (success)
+ */
+
+int main(void)
+{
+	int i =0;
+	char a = a ;
+	for ( i ; i<26 ; i++)
+	{
+		putchar(a) ;
+		a++;
+	}
+	return 0 ;
+}

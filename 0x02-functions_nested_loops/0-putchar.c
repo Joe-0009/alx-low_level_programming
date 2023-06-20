a26df52c3d38f6b9b1f29d9ed15ce890bb28746a
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-20 01:36:49 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+
+/**
+ * main - enttry point
+ *
+ * Description : display "_putchar"
+ *
+ * Return : always 0 (Success)
+ */
+
+
+int main(void){
+	
+	char str [] = "_putchar";
+	int i;
+
+	for(i =0 ; i<8 ; i++){
+		_putchar("_putchar");	
+	}
+	_putchar("\n");
+
+return (0);
+
+}

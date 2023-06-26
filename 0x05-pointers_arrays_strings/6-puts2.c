fb8b9b9be92e664618412d1e4c2f1fa0abe56056
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-26 14:41:46 -0700

    kf

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character
+ *
+ * @str: input string
+ */
+
+
+void puts2(char *str)
+{
+	int i, l;
+
+	i = 0;
+	l = 0;
+
+	while (str[i] != '\0')
+	{
+		l++;
+	}
+
+	while (i < l)
+	{
+		if (i % 2 != 0)
+			printf("%c", str[i]);
+	}
+	printf("\n");
+}

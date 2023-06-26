3b409366eca6846278d0cb5985e26442b70396df
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-26 10:38:32 -0700

    ll

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: input
+ *
+ */
+
+
+void rev_string(char *s)
+{
+	int len = strlen(s) - 1;
+	int i =0;
+	char str[len];
+
+	while (i <= len)
+	{
+		
+		str[i] = s[len];
+		i++;
+		len--;
+	}
+	str[i] = '\0';
+
+	return (str);
+}

49e5af0e3cf9402ff1432af6d484cf3fa2a7bfe2
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-26 15:40:53 -0700

    kf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n ; i++)
-		printf("%d, ", a[i]);
-	_putchar('\n');
+	for (i = 1; i <= n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n)
+			printf(", ");
+		
+	}
+	printf("\n");
 }

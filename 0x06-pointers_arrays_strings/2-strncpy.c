8db18412d0a1af301991ae12a163cd56137ab5aa
Joe-0009 <youssefrachidi1994@gmail.com>
2023-06-29 13:37:50 -0700

    hgh

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#iinclude "main.h"
+
+/**
+ * _strncpy - copies a string 
+ *
+ * @dest: string 1 inpiut
+ * @src: string 2 input
+ * @n: number input
+ *
+ * Return: string
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *arr;
+	int i;
+
+	arr = dest;
+
+	for (i = 0; i <= n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}

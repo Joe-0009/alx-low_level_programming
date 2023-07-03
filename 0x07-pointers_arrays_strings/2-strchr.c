cdc594cf6d6eea25aaca6053e535e294aa38e8e4
Joe-0009 <youssefrachidi1994@gmail.com>
2023-07-03 13:47:12 -0700

    gg

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,20 @@
  * Return: a pointer to the first occurrence of the character
  * c in the string s, or NULL if the character is not found
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
+		int a;
 
-		s++;
-	}
-	return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }

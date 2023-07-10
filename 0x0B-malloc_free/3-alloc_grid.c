1f9f397f67eff8a9c4cb4feb3b0ce13548c4e329
Joe <youssefrachidi1994@gmail.com>
2023-07-10 13:10:37 -0700

    amazaing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,14 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	p = (int *)calloc(height, sizeof(int *));
+	p = calloc(height, sizeof(int *));
 
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = (int *)calloc(width, (sizeof(int)));
+		p[i] = calloc(width, (sizeof(int)));
 	}
 
 	return (p);

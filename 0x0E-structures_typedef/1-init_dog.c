370b676e0260bbf456a165cada5080d1c2c33ed3
Joe <youssefrachidi1994@gmail.com>
2023-07-17 07:59:41 -0700

    gg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+
+
+/**
+ * init_dog - function that initialize a variable of type struct
+ *
+ * @d: object name
+ * @name: name
+ * @age: age
+ * @owner: owners name
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strncpy(dog->name, name, sizeof(dog->name) - 1);
+	dog->name[sizeof(dog->name) - 1] = '\0';
+
+	dog->age = age;
+
+	strncpy(dog->owner, owner, sizeof(dog->owner) - 1);
+	dog->owner[sizeof(dog->name) - 1] = '\0';
+}

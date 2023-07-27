60acc6805f7f3f4fb69807b03623ee209027e30c
joe-0009 <youssefrachidi1994@gmail.com>
2023-07-27 15:43:28 -0700

    gg

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * print_list - print elements in a linked list
+ *
+ * @h: pointer to head first node
+ *
+ * Return: Number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	
+}

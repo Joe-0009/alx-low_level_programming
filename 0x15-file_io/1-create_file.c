b2ecfccb63d7c752cccb9ee0d3d552437b1c27a7
joe-0009 <youssefrachidi1994@gmail.com>
2023-08-07 14:38:16 -0700

    gg

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	fo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fo == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
 		while (text_content[wc] != '\0')
 			wc++;

d08799ba078efae4bd3303c28a3394c41bed46d2
joe-0009 <youssefrachidi1994@gmail.com>
2023-08-30 06:19:15 -0700

    string length

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+
+
+
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y.
+ *
+ * @x: the number to power
+ * @y:the number to power to
+ *
+ * Return: powred number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

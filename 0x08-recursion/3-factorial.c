74e5d9ac3d08bf7de5a8ee4e125313ba372afe13
joe-0009 <youssefrachidi1994@gmail.com>
2023-08-30 06:06:52 -0700

    string length

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,8 +9,13 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
-	if ( n == 0)
+	} else if (n == 0)
+	{
 		return (1);
-	return (factorial(n - 1));
+	} else
+	{
+		return (factorial(n - 1));
+	}
 }

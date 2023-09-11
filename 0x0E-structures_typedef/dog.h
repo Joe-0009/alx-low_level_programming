01e6482ca4ee1f16559dfd59182dfe6cb07ecb57
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-11 07:57:53 -0700

    first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog infos
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: just a struct for dogs
+ */
+
+
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif

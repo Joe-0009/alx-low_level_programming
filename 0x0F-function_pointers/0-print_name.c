bc2931a508fa823d3649dfbff8051428014e60ca
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-13 14:25:12 -0700

    gg

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,15 +7,9 @@
  * Return: Always 0 (Success)
 */
 
-
-
-
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return (NULL);
-
-	f(name);
-
+	if (name && f)
+		f(name);
 }
+

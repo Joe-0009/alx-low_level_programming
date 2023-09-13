3dc48d3897ca83893016b28a1a5c2c96a9d303e2
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-13 14:23:02 -0700

    gg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+/**
+ * print_name - function to print name
+ *
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return (NULL);
+
+	f(name);
+
+}

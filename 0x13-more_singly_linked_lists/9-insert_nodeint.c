3caea585ae6fe6b4ddccf655573bf955e35a6700
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-27 12:39:28 -0700

    attempt number6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,10 +13,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *temp;
+	listint_t *temp = *head;
 	int i = 0;
 
-	*temp = *head;
 
 	if (new_node == NULL)
 		return (NULL);
@@ -27,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (i < idx && temp != NULL)
+	while (i < idx - 1 && temp != NULL)
 	{
 		*temp = *temp->next;
 		i++;

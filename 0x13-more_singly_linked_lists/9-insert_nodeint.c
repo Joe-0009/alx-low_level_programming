87e12bc52f77030c00d081feb781588d8a3febdc
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-27 12:33:14 -0700

    attempt number5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (i < idx - 1 && temp != NULL)
+	while (i < idx && temp != NULL)
 	{
 		*temp = *temp->next;
 		i++;

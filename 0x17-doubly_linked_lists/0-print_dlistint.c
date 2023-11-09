e3f18c2c655f6d7c83518cbcca0908ec5d601e7b
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-09 02:09:50 -0800

    first task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,6 +12,13 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *curent = *h;
 	size_t count = 0;
 
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	
 	while (current != NULL)
 	{
 		printf("%d%", current->n);

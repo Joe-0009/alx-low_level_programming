a7aa8e4b769b0da56eb2c5c56b993f64b5fed137
Joe-0009 <youssefrachidi1994@gmail.com>
2024-05-22 13:16:22 -0700

    second task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,64 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array of integers
- * using the Binary search algorithm
+ * print_array - prints an array of integers
+ * @array: pointer to the start of the array
+ * @size: size of the array (number of elements)
  *
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
+ * Return: nothing
+ */
+void print_array(int *array, int size)
+{
+	int i;
+
+	if (array == NULL || size == 0)
+		return;
+
+	printf("Searching in array: ");
+
+	for (i = 0; i < size; i++)
+	{
+		if (i == 0)
+			printf("%d", array[i]);
+		else
+			printf(", %d", array[i]);
+	}
+
+	printf("\n");
+}
+
+/**
+ * binary_search - find value in sorted array using binary search method
+ * @array: pointer to first element in array to be searched
+ * @size: size of the array (number of elements)
+ * @value: value to be searched for
  *
- * Return: Index where value is located, or -1 if not found or array is NULL
+ * Return: index position of value or -1 if not found or array is null
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid;
+	int low = 0;
+	int high = size - 1;
+	int mid;
 
-	if (array == NULL)
+	if (array == NULL || size == 0 || array[0] > value)
 		return (-1);
 
-	while (left <= right)
+	while (low <= high)
 	{
-		mid = left + (right - left) / 2;
+		mid = low + (low + high) / 2;
 
-		printf("Searching in array: ");
-		for (size_t i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i != right)
-				printf(", ");
-		}
-		printf("\n");
+		print_array(&array[low], (high - low) + 1);
 
 		if (array[mid] == value)
 			return (mid);
 
-		if (array[mid] < value)
-			left = mid + 1;
+		if (array[mid] > value)
+			high = mid - 1;
+
 		else
-			right = mid - 1;
+			low = mid + 1;
 	}
+
 	return (-1);
 }
